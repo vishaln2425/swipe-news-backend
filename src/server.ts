c2b5ea6f1b7efc333441dc81f9a227c2c3848d8d
Vishal Nikam <vishaln2425@gmail.com>
2025-07-02 18:29:59 +0530

    Your update message here

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 // src/server.ts
 import app from './app';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
