7b7a44e6f6784f195f8efd5f2ba378f1dd15f749
Vishal Nikam <vishaln2425@gmail.com>
2025-07-02 19:08:31 +0530

    Your update message here

--- src/data/dummyNews.ts
+++ src/data/dummyNews.ts
@@ -4,25 +4,26 @@ export const news = [
   {
     id: '1',
     title: 'Breaking News 1',
-    imageUrl: `${BASE_URL}/public/news/news1.jpeg`,
-    content: 'First news content'
+    imageUrl: 'https://swipe-news-backend-vccj.onrender.com/public/converted/news1.webp',
+    content: 'First news content',
   },
   {
     id: '2',
     title: 'Breaking News 2',
-    imageUrl: `${BASE_URL}/public/news/news2.jpeg`,
-    content: 'Second news content'
+    imageUrl: 'https://swipe-news-backend-vccj.onrender.com/public/converted/news2.webp',
+    content: 'Second news content',
   },
   {
     id: '3',
     title: 'Breaking News 3',
-    imageUrl: `${BASE_URL}/public/news/news3.png`,
-    content: 'Third news content'
+    imageUrl: 'https://swipe-news-backend-vccj.onrender.com/public/converted/news3.webp',
+    content: 'Third news content',
   },
   {
     id: '4',
     title: 'Breaking News 4',
-    imageUrl: `${BASE_URL}/public/news/news4.webp`,
-    content: 'Fourth news content'
+    imageUrl: 'https://swipe-news-backend-vccj.onrender.com/public/converted/news4.webp',
+    content: 'Fourth news content',
   }
 ];
+
